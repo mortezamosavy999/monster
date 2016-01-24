7a0974463c878c5c92ffce54423b11c4ed1bd7de
MOSAVI17 <www.mosavi7857@gmail.com>
2016-01-24 18:47:35 +0330

    Create Leave_add.lua

---
+++ plugins/Leave_add.lua
@@ -0,0 +1,43 @@
+do
+
+-- Will leave the group if be added
+
+local function run(msg, matches)
+
+local bot_id = our_id -- your bot id
+
+   -- like local bot_id = 157337513
+
+    if matches[1] == 'leave' and is_sudo(msg) then
+
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+
+    elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+
+       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
+
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+
+    end
+
+end
+
+ 
+
+return {
+
+  patterns = {
+
+    "^[!/$&-=+:*.%#?@](leave)$",
+
+    "^(leave)$",
+
+    "^!!tgservice (.+)$",
+
+  },
+
+  run = run
+
+}
+
+end
